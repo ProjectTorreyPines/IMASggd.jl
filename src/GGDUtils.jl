d69cd572f87470dc185d0a943e87908b7ff2ceaf
Anchal Gupta <guptaa@fusion.gat.com>
2023-08-23 13:20:24 -0700

    Overloaded interp to allow storing common kdtree
    One instance of interp now takes property object and kdtree object
    as inputs to generate interpolation function. kdtree can be
    created for a space using get_kdtree function and can be used for
    several properties on the same space. See test_interp function
    for example of how this case is used. Eventually, we should make an
    option of storing a kdtree in space object in IMAS data structure.

--- src/GGDUtils.jl
+++ src/GGDUtils.jl
@@ -1,29 +1,39 @@
 module GGDUtils
 
 import NearestNeighbors: KDTree, knn
-import SOLPS2IMAS: path_to_obj
 import StaticArrays: SVector
 import Statistics: mean
 
 export interp
+export get_kdtree
 
-function interp(ids, path_to_data, path_to_opd)
-    prop = path_to_obj(ids, path_to_data)
-    grid_nodes = path_to_obj(ids, path_to_opd)[1].object
-    grid_faces = path_to_obj(ids, path_to_opd)[3].object
+
+function get_kdtree(objects_per_dimension)
+    grid_nodes = objects_per_dimension[1].object
+    grid_faces = objects_per_dimension[3].object
     grid_faces = [cell for cell in grid_faces if length(cell.nodes) == 4]
     grid_centers = [SVector{2}(mean([grid_nodes[node].geometry for node in cell.nodes])) for cell in grid_faces]
-    kdtree = KDTree(grid_centers; leafsize=10)
+    return KDTree(grid_centers; leafsize=10)
+end
+
+
+function interp(prop, kdtree::KDTree)
     function get_interp_val(x, y)
         nearest_indices, distances = knn(kdtree, Array([x, y]), 4)
         v1, v2, v3, v4 = [prop[ii] for ii in nearest_indices]
         d1, d2, d3, d4 = distances
         return ((v1 * d2 * d3 * d4 + d1 * v2 * d3 * d4
                  + d1 * d2 * v3 * d4 + d1 * d2 * d3 * v4)
-                / (d2 * d3 * d4 + d1 * d3 * d4
-                   + d1 * d2 * d4 + d1 * d2 * d3))
+                /
+                (d2 * d3 * d4 + d1 * d3 * d4
+                 + d1 * d2 * d4 + d1 * d2 * d3))
     end
     return get_interp_val
+end
+
+
+function interp(prop, objects_per_dimension)
+    return interp(prop, get_kdtree(objects_per_dimension))
 end
 
 end # module GGDUtils
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,22 +1,45 @@
-import GGDUtils: interp
+import GGDUtils: interp, get_kdtree
 import OMAS: h5i2imas
 import Statistics: mean
 using Test
 
 function test_interp()
     ids = h5i2imas("$(@__DIR__)/../samples/edge_profiles.h5")
-    path_to_data = ["edge_profiles", "ggd", 1, "electrons", "density", 1, "values"]
-    path_to_opd = ["edge_profiles", "grid_ggd", 1, "space", 1, "objects_per_dimension"]
-    get_electron_density = interp(ids, path_to_data, path_to_opd)
+    electron_density = ids.edge_profiles.ggd[1].electrons.density[1].values
+    objects_per_dimension = ids.edge_profiles.grid_ggd[1].space[1].objects_per_dimension
+    # path_to_data = ["edge_profiles", "ggd", 1, "electrons", "density", 1, "values"]
+    # path_to_opd = ["edge_profiles", "grid_ggd", 1, "space", 1, "objects_per_dimension"]
+    get_electron_density = interp(electron_density, objects_per_dimension)
     chosen_index = 555
     nodes = ids.edge_profiles.grid_ggd[1].space[1].objects_per_dimension[3].object[chosen_index].nodes
     nodes_coords = [ids.edge_profiles.grid_ggd[1].space[1].objects_per_dimension[1].object[node].geometry for node in nodes]
     cell_center = mean(nodes_coords)
     grid_val = ids.edge_profiles.ggd[1].electrons.density[1].values[chosen_index]
     searched_val = get_electron_density(cell_center...)
+    println("Electron density at: ", cell_center)
     println("Grid Value: ", grid_val)
     println("Searched Value: ", searched_val)
     @assert(grid_val == searched_val)
+
+    # Use the same kdtree to interpolate other quantities
+    kdtree = get_kdtree(objects_per_dimension)
+    get_electron_temperature = interp(ids.edge_profiles.ggd[1].electrons.temperature[1].values, kdtree)
+    get_ion_density = interp(ids.edge_profiles.ggd[1].ion[1].density[1].values, kdtree)
+
+    grid_val = ids.edge_profiles.ggd[1].electrons.temperature[1].values[chosen_index]
+    searched_val = get_electron_temperature(cell_center...)
+    println("Electron temperature at: ", cell_center)
+    println("Grid Value: ", grid_val)
+    println("Searched Value: ", searched_val)
+    @assert(grid_val == searched_val)
+
+    grid_val = ids.edge_profiles.ggd[1].ion[1].density[1].values[chosen_index]
+    searched_val = get_ion_density(cell_center...)
+    println("First Ion density at: ", cell_center)
+    println("Grid Value: ", grid_val)
+    println("Searched Value: ", searched_val)
+    @assert(grid_val == searched_val)
+    
     return true
 end
 
