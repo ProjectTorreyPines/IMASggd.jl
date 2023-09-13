7131e1de75ec7184495852317fcf789912d15e4e
Anchal Gupta <guptaa@fusion.gat.com>
2023-09-12 20:09:06 -0700

    Added project_prop_on_subset as a slicing function
    project_prop_on_subset can be used to project values of any property
    like dd.edge_profiles.ggd[1].electrons.density onto another subset.
    Desired subset, if not present, can be generated using functions
    available in SOLPS2IMAS.
    
    This requried function call change of interp. It makes more sense to
    give it a property object rather than just the values.

--- src/GGDUtils.jl
+++ src/GGDUtils.jl
@@ -3,14 +3,17 @@ module GGDUtils
 import NearestNeighbors: KDTree, knn
 import StaticArrays: SVector
 import Statistics: mean
+import OMAS
+import SOLPS2IMAS: get_subset_space
 
 export interp
 export get_kdtree
+export project_prop_on_subset!
 
 
-function get_kdtree(objects_per_dimension)
-    grid_nodes = objects_per_dimension[1].object
-    grid_faces = objects_per_dimension[3].object
+function get_kdtree(space::OMAS.edge_profiles__grid_ggd___space)
+    grid_nodes = space.objects_per_dimension[1].object
+    grid_faces = space.objects_per_dimension[3].object
     grid_faces = [cell for cell in grid_faces if length(cell.nodes) == 4]
     grid_centers = [SVector{2}(mean([grid_nodes[node].geometry for node in cell.nodes])) for cell in grid_faces]
     return KDTree(grid_centers; leafsize=10)
@@ -18,9 +21,9 @@ end
 
 
 function interp(prop, kdtree::KDTree)
-    function get_interp_val(x, y)
+    function get_interp_val(x::Number, y::Number)
         nearest_indices, distances = knn(kdtree, Array([x, y]), 4)
-        v1, v2, v3, v4 = [prop[ii] for ii in nearest_indices]
+        v1, v2, v3, v4 = [prop.values[ii] for ii in nearest_indices]
         d1, d2, d3, d4 = distances
         return ((v1 * d2 * d3 * d4 + d1 * v2 * d3 * d4
                  + d1 * d2 * v3 * d4 + d1 * d2 * d3 * v4)
@@ -28,12 +31,59 @@ function interp(prop, kdtree::KDTree)
                 (d2 * d3 * d4 + d1 * d3 * d4
                  + d1 * d2 * d4 + d1 * d2 * d3))
     end
+    function get_interp_val(x::Vector{Float64}, y::Vector{Float64})
+        if length(x) != length(y)
+            error("Length of the two axes are not equal")
+        else
+            return [get_interp_val(x[ii], y[ii]) for ii in eachindex(x)]
+        end
+    end
+    function get_interp_val(xy::Vector{Tuple{Float64, Float64}})
+        return [get_interp_val(xy[ii]...) for ii in eachindex(xy)]
+    end
     return get_interp_val
 end
 
 
-function interp(prop, objects_per_dimension)
-    return interp(prop, get_kdtree(objects_per_dimension))
+function interp(prop, space::OMAS.edge_profiles__grid_ggd___space)
+    return interp(prop, get_kdtree(space))
+end
+
+
+function project_prop_on_subset!(prop, from_subset, to_subset;
+                                 space::OMAS.edge_profiles__grid_ggd___space)
+    from_prop = nothing
+    for p in prop
+        if p.grid_subset_index == from_subset.identifier.index
+            from_prop = p
+            break
+        end
+    end
+    if isnothing(from_prop)
+        println("from_subset not represented in the property yet")
+        return false
+    end
+    
+    if from_subset.element[1].object[1].dimension == to_subset.element[1].object[1].dimension
+        resize!(prop, length(prop) + 1)
+        to_prop = prop[end]
+        to_prop.grid_index = from_prop.grid_index
+        to_prop.grid_subset_index = to_subset.identifier.index
+        from_subset_element_inds = [ele.index for ele in from_subset.element]
+        filtered_values = [from_prop[ele.index] for ele in to_subset.element if ele.index ∈ from_subset_element_inds]
+        resize!(to_prop.values, length(filtered_values)) 
+        to_prop.values = filtered_values
+    elseif from_subset.element[1].object[1].dimension > to_subset.element[1].object[1].dimension
+        resize!(prop, length(prop) + 1)
+        to_prop = prop[end]
+        to_prop.grid_index = from_prop.grid_index
+        to_prop.grid_subset_index = to_subset.identifier.index
+        resize!(to_prop.values, length(to_subset.element))
+        prop_interp = interp(from_prop, space)
+        grid_nodes = space.objects_per_dimension[1].object
+        to_subset_space = get_subset_space(space, to_subset.element)
+        to_prop.values = prop_interp([Tuple(mean([grid_nodes[node].geometry for node in obj.nodes])) for obj in to_subset_space])
+    end
 end
 
 end # module GGDUtils
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,18 +1,19 @@
-import GGDUtils: interp, get_kdtree
+import GGDUtils: interp, get_kdtree, project_prop_on_subset!
 import OMAS: h5i2imas
 import Statistics: mean
+import SOLPS2IMAS: solps2imas, get_grid_subset_with_index
 using Test
 
 function test_interp()
     ids = h5i2imas("$(@__DIR__)/../samples/edge_profiles.h5")
-    electron_density = ids.edge_profiles.ggd[1].electrons.density[1].values
-    objects_per_dimension = ids.edge_profiles.grid_ggd[1].space[1].objects_per_dimension
+    electron_density = ids.edge_profiles.ggd[1].electrons.density[1]
+    space = ids.edge_profiles.grid_ggd[1].space[1]
     # path_to_data = ["edge_profiles", "ggd", 1, "electrons", "density", 1, "values"]
     # path_to_opd = ["edge_profiles", "grid_ggd", 1, "space", 1, "objects_per_dimension"]
-    get_electron_density = interp(electron_density, objects_per_dimension)
+    get_electron_density = interp(electron_density, space)
     chosen_index = 555
-    nodes = ids.edge_profiles.grid_ggd[1].space[1].objects_per_dimension[3].object[chosen_index].nodes
-    nodes_coords = [ids.edge_profiles.grid_ggd[1].space[1].objects_per_dimension[1].object[node].geometry for node in nodes]
+    nodes = space.objects_per_dimension[3].object[chosen_index].nodes
+    nodes_coords = [space.objects_per_dimension[1].object[node].geometry for node in nodes]
     cell_center = mean(nodes_coords)
     grid_val = ids.edge_profiles.ggd[1].electrons.density[1].values[chosen_index]
     searched_val = get_electron_density(cell_center...)
@@ -22,9 +23,9 @@ function test_interp()
     @assert(grid_val == searched_val)
 
     # Use the same kdtree to interpolate other quantities
-    kdtree = get_kdtree(objects_per_dimension)
-    get_electron_temperature = interp(ids.edge_profiles.ggd[1].electrons.temperature[1].values, kdtree)
-    get_ion_density = interp(ids.edge_profiles.ggd[1].ion[1].density[1].values, kdtree)
+    kdtree = get_kdtree(space)
+    get_electron_temperature = interp(ids.edge_profiles.ggd[1].electrons.temperature[1], kdtree)
+    get_ion_density = interp(ids.edge_profiles.ggd[1].ion[1].density[1], kdtree)
 
     grid_val = ids.edge_profiles.ggd[1].electrons.temperature[1].values[chosen_index]
     searched_val = get_electron_temperature(cell_center...)
@@ -39,10 +40,33 @@ function test_interp()
     println("Grid Value: ", grid_val)
     println("Searched Value: ", searched_val)
     @assert(grid_val == searched_val)
-    
+
+    chosen_index = 553:557
+    grid_val = ids.edge_profiles.ggd[1].ion[1].density[1].values[chosen_index]
+    chosen_nodes = [space.objects_per_dimension[3].object[ii].nodes for ii in chosen_index]
+    cell_centers = [Tuple(mean([space.objects_per_dimension[1].object[node].geometry for node in nodes])) for nodes in chosen_nodes]
+    searched_val = get_ion_density(cell_centers)
+    return true
+end
+
+
+function test_project_prop_on_subset()
+    b2gmtry = "$(@__DIR__)/../samples/b2fgmtry"
+    b2output = "$(@__DIR__)/../samples/b2time_red.nc"
+    gsdesc = "$(@__DIR__)/../samples/gridspacedesc.yml"
+    b2mn = "$(@__DIR__)/../samples/b2mn.dat"
+    dd = solps2imas(b2gmtry, b2output, gsdesc, b2mn)
+    space = dd.edge_profiles.grid_ggd[1].space[1]
+    prop = dd.edge_profiles.ggd[1].electrons.density
+    from_subset = get_grid_subset_with_index(dd.edge_profiles.grid_ggd[1], 5)   # All cells
+    to_subset = get_grid_subset_with_index(dd.edge_profiles.grid_ggd[1], 16)  # separatix
+    # Cheating: need to fix solps2imas, prop did not get correct grid_subset_index
+    prop[5].grid_subset_index = 5
+    project_prop_on_subset!(prop, from_subset, to_subset; space)
     return true
 end
 
 @testset "GGDUtils" begin
     @test test_interp()
+    @test test_project_prop_on_subset()
 end
